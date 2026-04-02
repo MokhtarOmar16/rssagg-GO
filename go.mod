dc8400dcb702bc5d103d2f78800890783dc22fb4
Mokhtar Omar <AlMokhtarOmar24@gmail.com>
2026-04-02 18:55:45 +0200

    hello world server

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/MokhtarOmar16/rssagg-GO
+
+go 1.22.1
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+	// "github.com/go-chi/cors"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	fmt.Println(os.Getenv("PORT"))
+	router := chi.NewRouter()
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + os.Getenv("PORT"),
+	}
+	log.Print("Starting server on port " + os.Getenv("PORT"))
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal("Error starting server")
+	}
 }
